88d282c92074be31610d462095d3d37d003282d6
Henning Meyer <h.meyer@affiliprint.de>
2015-02-06 17:36:52 +0100

    voting works now

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -1,21 +1,40 @@
-from django.http import HttpResponse
-from polls.models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+# from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from polls.models import Choice, Question
+from django.core.urlresolvers import reverse
+
+
+def vote(request, question_id):
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': p,
+			'error_message': "You didnt select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ','.join([p.question_text for p in latest_question_list])
-	return HttpResponse(output)
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-	return HttpResponse("Youre looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-	response = "Youre looking at the results of Question %s."
-	return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-	return HttpResponse("Youre voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})

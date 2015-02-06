4143429b6c23047c53b8c4b57b97a85033eddf3d
Henning Meyer <h.meyer@affiliprint.de>
2015-02-06 16:21:12 +0100

    added views

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from polls.models import Question
 
-# Create your views here.
+
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ','.join([p.question_text for p in latest_question_list])
+	return HttpResponse(output)
+
+
+def detail(request, question_id):
+	return HttpResponse("Youre looking at question %s." % question_id)
+
+
+def results(request, question_id):
+	response = "Youre looking at the results of Question %s."
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("Youre voting on question %s." % question_id)

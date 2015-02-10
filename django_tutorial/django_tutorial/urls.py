22298567234b04cfc6babab31e01208c99a38061
Henning Meyer <h.meyer@affiliprint.de>
2015-02-10 14:15:13 +0100

    added tests

--- django_tutorial/django_tutorial/urls.py
+++ django_tutorial/django_tutorial/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
 	url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 )
